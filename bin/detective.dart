79d33e9995087e7721e777774075d4dd2bbba0a8
Norbert515 <kozsir.norbert@gmail.com>
2021-02-18 14:51:25 +0100

    Remove the need for disable auth codes in prompt

--- bin/detective.dart
+++ bin/detective.dart
@@ -10,7 +10,7 @@ void main(List<String> args) async {
   var file = File(Directory.current.uri.resolve('detective_connect.txt').toFilePath(windows: Platform.isWindows));
 
   if(!await file.exists()) {
-    print('You app doesn\'t seem to be running or you didn\'t launch it with the "--vmservice-out-file=detective_connect.txt --disable-service-auth-codes" arguments\n'
+    print('You app doesn\'t seem to be running or you didn\'t launch it with the "--vmservice-out-file=detective_connect.txt" argument\n'
         '\n'
         'Make sure that "${file.path}" exists after you launched your app.');
     exit(22);
